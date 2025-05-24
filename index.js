e05fbdd3771fccd9006adf359c8933a978319717
Kritika Walia <kritikawalia987@gmail.com>
2025-05-24 14:00:10 +0530

    Added eventLogger.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const Hello = require('./hello');
+const logger = require('./eventsLogger');
+
+logger.log('User clicked on Login');
+logger.log('User signed up');
+
 const person = new Hello("All");
 console.log(person.sayHello());
