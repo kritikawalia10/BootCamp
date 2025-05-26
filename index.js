112303ab2816bda9888df4a948c440bf2ee92c84
Kritika Walia <kritikawalia987@gmail.com>
2025-05-26 14:14:30 +0530

    Used Chalk package

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const Hello = require('./hello');
 const logger = require('./eventsLogger');
+const chalk = require('chalk');
 
-logger.log('User clicked on Login');
-logger.log('User signed up');
+logger.log(chalk.blue('User clicked on Login'));
+logger.log(chalk.green('User signed up'));
 
 const person = new Hello("All");
-console.log(person.sayHello());
+console.log(chalk.yellow(person.sayHello()));
